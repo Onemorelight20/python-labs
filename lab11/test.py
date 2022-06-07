79e3a528b6f9f1ff25ccc7692ec508a1d1919ae2
Bohdan Boretskyi <bogdanbor.etc@gmail.com>
2022-06-07 16:08:22 +0300

    minor improvements

--- lab11/test.py
+++ lab11/test.py
@@ -10,16 +10,10 @@ date_to = datetime.strptime('10/Mar/2004:11:41:52', date_format)
 file1 = open(file_name, 'r')
 lines = file1.readlines()
 
-count = 0
 result_dict = {}
-# Strips the newline character
+
 for line in lines:
-    count += 1
     splitted_line = re.split(pattern, line)
-
-    print(splitted_line)
-    print(count, line.strip())
-
     device_initiator = splitted_line[0]
     date_time_str = splitted_line[1]
     date_time_of_log = datetime.strptime(date_time_str, date_format)
